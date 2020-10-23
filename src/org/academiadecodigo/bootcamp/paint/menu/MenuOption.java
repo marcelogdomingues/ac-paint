a83f9681cb8069cf0c69f0f7189f18a7253f1049
marcelogdomingues <marcelo.domingues.ul@gmail.com>
2020-10-23 21:54:08 +0100

    README.md added

---
+++ src/org/academiadecodigo/bootcamp/paint/menu/MenuOption.java
@@ -0,0 +1,14 @@
+package org.academiadecodigo.bootcamp.paint.menu;
+
+public enum MenuOption {
+
+    START(1),
+    INSTRUCTIONS(2),
+    CREDITS(3),
+    EXIT(4),
+    ;
+
+    MenuOption(int i) {
+    }
+}
+
